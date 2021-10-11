db5b17f5ec8d82d94d7518d8259910b068141656
Mikhail Surin <surinmike@gmail.com>
2021-10-11 19:47:01 +0300

    deps.edn & tools.build

---
+++ build.clj
@@ -0,0 +1,40 @@
+(ns build
+  (:require [clojure.tools.build.api :as b]))
+
+(def lib 'com.github.ssmike/tag-search)
+(def main 'tag-search.service)
+
+(def version "0.1.0-SNAPSHOT")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+(def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn jar [_]
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn compile [_]
+  (b/compile-clj {:basis basis
+                  :src-dirs ["src"]
+                  :class-dir class-dir}))
+
+(defn uber [_]
+  (clean nil)
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+
+  (b/uber {:class-dir class-dir
+           :uber-file uber-file
+           :main main
+           :basis basis}))
