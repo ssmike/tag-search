7325bfbad6963b59a342edb207722a5f93857dc3
Mikhail Surin <surinmike@gmail.com>
2021-12-11 14:55:49 +0300

    up vulnerable log4j dep

--- project.clj
+++ project.clj
@@ -11,7 +11,8 @@
                  [org/jaudiotagger "2.0.3"]
                  [org.clojure/tools.cli "1.0.206"]
                  [org.clojure/data.json "2.3.0"]
-                 [org.clojure/tools.logging "1.1.0"]]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [org.apache.logging.log4j/log4j-core "2.15.0"]]
   :main tag-search.service
   :aot [tag-search.service]
   :repl-options {:init-ns tag-search.service})
